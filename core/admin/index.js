b1c15a2e9f98392ddb1ccc675c8913fc3862f15f
Jason Shin <visualbbasic@gmail.com>
2017-02-12 21:26:03 +1100

    feat: socket io implemented

--- core/admin/index.js
+++ core/admin/index.js
@@ -5,6 +5,7 @@ import exphbs from 'express-handlebars'
 import http from 'http'
 import morgan from 'morgan'
 import path from 'path'
+import SocketIO from 'socket.io'
 import api from './controllers/api'
 import html from './controllers/html'
 import config from '../../config'
@@ -13,13 +14,15 @@ import middleware from './middleware'
 
 // Initiating express
 const app = express()
+const server = http.Server(app)
+const io = SocketIO(server)
 const viewPath = path.join(__dirname, 'views')
 const hbs = exphbs({
 	layoutsDir: viewPath,
 	extname: '.hbs',
 })
 
-app.server = http.createServer(app)
+app.server = server
 
 // Express configuration
 app.set('port', config.get('web:port') || 3000)
@@ -56,30 +59,34 @@ initializeDb((db) => {
   // api router
 	app.use('/api', api({ config, db }))
 
+	io.on('connection', (socket) => {
+		console.log('client connected ', socket)
+		socket.on('event', (data) => {
+			console.log('data received ', data)
+		})
+		socket.on('disconnect', () => {
+			console.log('socket disconnected')
+		})
+	})
+
   /**
    * Start Express server.
    * TODO: Instead of callback try incorporating promises using bluebird.js
    */
-	app.listen(app.get('port'), () => {
-		// console.log('%s App is running at http://localhost:%d in %s mode ', app.get('port'))
-		// console.log('  Press CTRL-C to stop\n')
+	server.listen(app.get('port'), () => {
+		console.log('%s App is running at http://localhost:%d in %s mode ', app.get('port'))
+		console.log('  Press CTRL-C to stop\n')
 	})
-	/*console.log('server instance')
-	console.log(app.server)
-	var server = require('http').createServer();
-	var io = require('socket.io')(server);
-	io.on('connection', function(client){
-		client.on('event', function(data){});
-		client.on('disconnect', function(){});
-	})0;*/
-	//server.listen(3000);
+
 })
-/*
+
 setTimeout(() => {
-	var socket = require('socket.io-client')('http://localhost:3000');
-	socket.on('connect', function(){});
+	var socket = require('socket.io-client')('http://localhost:8080/')
+	socket.on('connect', function(){
+		console.log('connected to the server!')
+	});
 	socket.on('event', function(data){});
 	socket.on('disconnect', function(){});
-}, 3000)*/
+}, 3000)
 
 export default app
