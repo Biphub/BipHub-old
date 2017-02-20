83bf142445b9beeb79e44c6e35c37d70e6b83018
Jason Shin <visualbbasic@gmail.com>
2017-02-20 21:17:11 +1100

    refactor: simplified promise in schema

--- core/data/schema/bips.schema.js
+++ core/data/schema/bips.schema.js
@@ -1,14 +1,14 @@
 import connection from '../db/connection'
 
-const createTable = () => new Promise((resolve, reject) => {
+async function createTable() {
   connection.knex.schema.createTableIfNotExists('bips', (table) => {
     table.increments()
     table.string('name')
     table.string('description', 128)
     table.timestamps()
-  }).then(() => resolve())
-  return reject()
-})
+  }).then(() => Promise.resolve())
+  return Promise.reject()
+}
 
 export default {
   createTable,
