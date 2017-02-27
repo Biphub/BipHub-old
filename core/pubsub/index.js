67c66fe37912f74773db58996867c3c08317887e
Jason Shin <visualbbasic@gmail.com>
2017-02-27 21:54:38 +1100

    chore: created empty methods

--- core/pubsub/index.js
+++ core/pubsub/index.js
@@ -1,2 +1,20 @@
 const nspBips = io.of('/bips')
 nspBips.on('connection', socket)
+
+const publish = () => {
+
+}
+
+const subscribe = () => {
+
+}
+
+const unsubscribe = () => {
+
+}
+
+export default {
+	publish,
+	subscribe,
+	unsubscribe,
+}
