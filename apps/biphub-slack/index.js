22ca68a5931f1f0a391c01c8bf82ce5e2097d449
Jason Shin <visualbbasic@gmail.com>
2017-03-12 13:30:02 +1100

    feat: implemented slackbots

--- apps/biphub-slack/index.js
+++ apps/biphub-slack/index.js
@@ -1,4 +1,5 @@
 import client from 'socket.io-client'
+import SlackBots from 'slackbots'
 import config from './config'
 
 function init() {
@@ -15,8 +16,39 @@ function init() {
     console.log('test pubsub from client')
   })
   // Register trello Bip
-  console.log('emitting!')
+  console.log('emitting! yo!')
   socket.emit('REGISTER_APP', config)
+
+  // Slack test
+  const bot = new SlackBots({
+    token: 'xoxb-153818245495-tokNO6F1RKrFj15IGXfZBnpQ',
+    name: 'biphubbot',
+  })
+
+  bot.on('start', () => {
+		// more information about additional params https://api.slack.com/methods/chat.postMessage
+    const params = {
+      icon_emoji: ':cat:',
+    }
+
+		// define channel, where bot exist. You can adjust it there https://my.slack.com/services
+    bot.postMessageToChannel('general', 'meow!', params)
+
+		// define existing username instead of 'user_name'
+    bot.postMessageToUser('visualbbasic', 'meow!', params)
+
+		// If you add a 'slackbot' property,
+		// you will post to another user's slackbot channel instead of a direct message
+    bot.postMessageToUser('user_name', 'meow!', { slackbot: true, icon_emoji: ':cat:' })
+
+		// define private group instead of 'private_group', where bot exist
+    bot.postMessageToGroup('private_group', 'meow!', params)
+  })
+
+  bot.on('message', (data) => {
+		// all ingoing events https://api.slack.com/rtm
+    console.log(data)
+  })
 }
 
 const getConfig = () => config
