df2393e9b3f0b0210c7985026a0bebf8115f4513
Jason Shin <visualbbasic@gmail.com>
2017-04-11 20:24:59 +1000

    feat: cleaner app card

--- client/js/components/App/App.vue
+++ client/js/components/App/App.vue
@@ -4,14 +4,15 @@
   <div class="container"
        v-on:click="onClick" >
     <div class="row">
-      <div class="icon">
+      <div class="icon" v-bind:style="appTheme">
         <span class="icon-wrapper">
           <img v-bind:src="src">
         </span>
       </div>
       <div class="label"
            v-if="label" >
-        {{ label }}
+        <h5>{{ label }}</h5>
+        <span>Lorem ipsum</span>
       </div>
       <div class="description"></div>
     </div>
@@ -19,11 +20,17 @@
 </template>
 
 <script>
+// gradient images https://uigradients.com
 export default {
   props: {
     src: { type: String, default: 'N/A' },
     label: { type: String, default: null },
-    border: { type: Boolean, default: false }
+    border: { type: Boolean, default: false },
+    theme: { type: String, default: 'orca' },
+  },
+  data() {
+    
+    return {}
   },
   computed: {
     iconBorder () {
@@ -32,6 +39,9 @@ export default {
       } else {
         return 'app-icon--no-border'
       }
+    },
+    appTheme() {
+      return `background-image: url(static/app_themes/${this.theme}.jpg)`
     }
   },
   methods: {
