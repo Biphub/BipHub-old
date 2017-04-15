1e294200218ece9d493fca4438869116996b3a60
Jason Shin <visualbbasic@gmail.com>
2017-04-15 16:27:00 +1000

    chore: adding vuex

---
+++ client/js/store/App/App.getters.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Shin on 15/04/2017.
+ */
---
+++ client/js/store/App/index.js
@@ -0,0 +1,14 @@
+import actions from './App.actions';
+import mutations from './App.mutations';
+import getters from './App.getters';
+
+const state = {
+  apps: [],
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
