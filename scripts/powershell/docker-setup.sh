65e005c98abda5e838c7f9099749f9c1765a098b
Jason Shin <visualbbasic@gmail.com>
2017-05-06 22:48:17 +1000

    added powershell setup script for docker

---
+++ scripts/powershell/docker-setup.sh
@@ -0,0 +1,4 @@
+echo "Setting COMPOSE_CONVERT_WINDOWS_PATH that converts win path to linux path. Required for volume share"
+$env:COMPOSE_CONVERT_WINDOWS_PATHS=1
+echo "Attempting to link default docker-machine to Powershell"
+docker-machine env --shell powershell default | Invoke-Expression
