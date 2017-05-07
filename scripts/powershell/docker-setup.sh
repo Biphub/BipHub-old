41f8a9d20c205caf5ff92b949fa1335111f4b824
Jason Shin <visualbbasic@gmail.com>
2017-05-07 10:54:18 +1000

    docker setup sh added default machine start

--- scripts/powershell/docker-setup.sh
+++ scripts/powershell/docker-setup.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+echo "docker machine starting default"
+docker-machine start
 echo "Setting COMPOSE_CONVERT_WINDOWS_PATH that converts win path to linux path. Required for volume share"
 $env:COMPOSE_CONVERT_WINDOWS_PATHS=1
 echo "Attempting to link default docker-machine to Powershell"
