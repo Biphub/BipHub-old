d67ea5f332a73e443ed1407f18cb600ddd68456a
Jason Shin <visualbbasic@gmail.com>
2017-05-28 17:32:59 +1000

    testing out few things around bip actions

--- core/actions/bip_actions.js
+++ core/actions/bip_actions.js
@@ -39,13 +39,16 @@ async function bip (appName, payload, socket) {
   )
   const incomingActions = await app.related('actions')
     .where({ name: meta.name, app_id: app.get('id'), type: 'incomingActions' })
+  console.log('found actions ', incomingActions)
   // Get first entity's id since meta.name can associate with only one incoming action
   // Received incoming action must be unique using action meta.name & app_id & type: incomingActions
   const firstIncActionId = R.head(incomingActions).get('id')
+  const firstIncActionName = R.head(incomingActions).get('name')
+  console.log('first inc action id ', firstIncActionId, ' name: ', firstIncActionName)
   // Find all bips that is associated with the unique incoming action
   const bips = await models.Bip
     .findAll({ incoming_action_id: firstIncActionId }, { withRelated: [] })
-  console.log('found bips ', bips)
+  console.log('found bips ', bips.toJSON())
   /* checkAllIncomingActionConditions({
     app, incomingAction, incomingActionPayload
   })
