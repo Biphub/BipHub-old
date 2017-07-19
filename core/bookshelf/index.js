d2131bc9620a2cfbbc66c7f7f25bf988be2ffa0a
Jason Shin <visualbbasic@gmail.com>
2017-07-19 22:02:29 +1000

    refactor all

--- core/bookshelf/index.js
+++ core/bookshelf/index.js
@@ -1,22 +1,68 @@
+import root from '../helpers/root'
+import R from 'ramda'
+import Fantasy from 'ramda-fantasy'
+import Knex from 'knex'
+import Bookshelf from 'bookshelf'
 import path from 'path'
 import knexCleaner from 'knex-cleaner'
 import knexConfig from './knexfile'
-import db from './db/connection'
+import config from '../../config'
+// import db from './db/connection'
+const Future = Fantasy.Future
 
 // Assigns migration and seed directory path for Knex
 const migrationConfig = Object.assign(knexConfig, { directory: path.join(__dirname, 'migrations'), debug: false })
 const seedConfig = { directory: path.join(__dirname, 'seeds'), debug: true }
-const connection = db()
-const { knex, bookshelf } = connection
+// const connection = db()
+// const { knex, bookshelf } = connection
 
-async function migrate(env) {
+function migrateLatest(bookshelf) {
+  return new Future((rej, res) => {
+    bookshelf.knex.migrate.latest(migrationConfig).then(() => {
+      return res(bookshelf)
+    })
+  })
+}
+
+function clean(bookshelf) {
+  return new Future((rej, res) => {
+    knexCleaner.clean(bookshelf.knex).then(() => {
+      return res(bookshelf)
+    })
+  })
+}
+
+function init() {
+  return new Future((rej, res) => {
+    if (typeof root.bookshelf === 'undefined') {
+      // TODO: Check if it needs config
+      root.knex = Knex(config.get('database'))
+      root.bookshelf = Bookshelf(root.knex)
+      root.bookshelf.plugin('registry')
+      return res(root.bookshelf)
+    }
+  })
+}
+
+function migrate() {
+  R.compose(
+
+    R.chain(clean),
+    init
+  )
+}
+
+/*
+async function migrate() {
   await knexCleaner.clean(knex)
   await knex.migrate.latest(migrationConfig)
-  if (env === 'test') {
+  if (process.env.NODE_ENV === 'test') {
 	  await knex.seed.run(seedConfig)
   }
   return true
 }
+*/
+
 export default {
   bookshelf,
   migrate,
--- core/test/unit/actions/app_actions.test.js
+++ core/test/unit/actions/app_actions.test.js
@@ -1,8 +1,17 @@
-/* global describe it */
+/* global describe it beforeEach */
 import assert from 'assert'
+import bookshelf from '../../../bookshelf'
 
-describe('Array', () => {
+require('babel-register')
+require('babel-polyfill')
+
+describe('App actions', () => {
   describe('#indexOf()', () => {
+    beforeEach((done) => {
+      console.log('before migration')
+      bookshelf.migrate().then(() => done())
+      console.log('checking migration!')
+    })
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1, 2, 3].indexOf(4))
     })
